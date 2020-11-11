ffdba3faec6e2b7e8e76c5254e57429d96b4797f
reejit <70316178+reejit@users.noreply.github.com>
2020-11-11 18:51:15 +0530

    Update forceSubscribe.py

--- plugins/forceSubscribe.py
+++ plugins/forceSubscribe.py
@@ -66,7 +66,7 @@ def _check_member(client, message):
                          InlineKeyboardButton(
                              text="Let Me Speak",
                              url=f"https://t.me/{SUPPORT_CHAT}")
-                     ])
+                     ]]
           client.restrict_chat_member(chat_id, user_id, ChatPermissions(can_send_messages=False))
         except ChatAdminRequired:
           sent_message.edit("❗ **I am not an admin here.**\n__Make me admin with ban user permission and add me again.\n#Leaving this chat...__")
